e898f45b0f5c0930049695d9c7d501eaf1c3e189
chengen0612 <chengen0612@gmail.com>
2022-07-28 11:29:48 +0800

    chore: separate webpack configuration into three parts, shared common config, dev and prod configs that take care of their own environments

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/main.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "source-map",
-  devServer: {
-    static: "./dist",
   },
   module: {
     rules: [
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,9 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+  },
 });
